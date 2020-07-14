dc72c652ac27d82782b3fb5f690d5201120036b9
weterperm <44847478+weterperm@users.noreply.github.com>
2020-07-14 22:43:03 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+
+anime({
+  targets: '.alpha',
+  translateY: -50,
+  direction: 'alternate',
+  loop: false,
+  easing: 'steps(1)'
+})
+
+anime({
+  targets: '.alpha',
+  translateY: 50,
+  direction: 'alternate',
+  loop: false,
+  easing: 'spring(1, 70, 7, 5)'
+})
+
+anime({
+  targets: '.name',
+  translateX: 220,
+  direction: 'alternate',
+  loop: false,
+  easing: 'spring(1, 50, 8, 1)'
+})
+
+
+
+
+
+
+
+
+
